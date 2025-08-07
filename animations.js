b1b874c5a4a28f913e43d6157272330c8da9b237
cgamcs <cesar.garciamcs@proton.me>
2025-08-07 11:53:04 -0600

    empezando transiciones - animacion al abrir

--- animations.js
+++ animations.js
@@ -1,10 +1,29 @@
-function toggleDialog(dialogId) {
-    if (!dialogId) {
-        const openDialog = document.querySelector('dialog[open]')
-        openDialog.close()
-
-        return
-    }
+async function toggleDialog(dialogId) {
+    const viewTransitionClass = 'vt-element-animation'
+    const viewTransitionClassClosing = 'vt-element-animation-closing'
+    
     const dialog = document.getElementById(dialogId)
-    dialog.showModal()
+    const originElement = event.currentTarget
+
+    dialog.style.viewTransitionName = 'vt-shared'
+    dialog.style.viewTransitionClass = viewTransitionClass
+
+    originElement.style.viewTransitionName = 'vt-shared'
+    originElement.style.viewTransitionClass = viewTransitionClass
+    originElement.setAttribute('origin-element', '')
+
+    const viewTransition = document.startViewTransition(() => {
+        originElement.style.viewTransitionName = ''
+        originElement.style.viewTransitionClass = ''
+        dialog.showModal()
+    })
+    await viewTransition.finished
+    
+    
+    // if (!dialogId) {
+    //     const openDialog = document.querySelector('dialog[open]')
+    //     openDialog.close()
+    
+    //     return
+    // }
 }
